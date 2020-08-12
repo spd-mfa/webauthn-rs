ebc51826d3a4f861f56f479314fd1dc1e7bb4052
William Brown <william@blackhats.net.au>
2020-08-13 09:39:46 +1000

    jquery version

--- static/js/webauthn.js
+++ static/js/webauthn.js
@@ -44,133 +44,118 @@ function toast_o_matic(message) {
 function register() {
   var username = document.getElementById("username").value;
 
-  fetch(REG_CHALLENGE_URL + username, {method: "POST"})
-    .then(res => {
-        // Handle error?
-        if (res.status != 200) {
-            toast_o_matic("Oppsie Whoopsie");
-            // Update a element on page to say we broke.
-            throw new Error("Opps");
-            // How to exit properly?
-        } else {
-            return res;
-        }
+  jQuery.post(REG_CHALLENGE_URL + username, null, null, 'json')
+    .done(function(challenge) {
+        console.log("challenge");
+        console.log(challenge);
+        challenge.publicKey.challenge = fromBase64(challenge.publicKey.challenge);
+        challenge.publicKey.user.id = fromBase64(challenge.publicKey.user.id);
+
+        navigator.credentials.create(challenge)
+            .then(newCredential => {
+                console.log("PublicKeyCredential Created");
+                console.log(newCredential);
+                console.log(typeof(newCredential));
+                const cc = {};
+                cc.id = newCredential.id;
+                cc.rawId = toBase64(newCredential.rawId);
+                cc.response = {};
+                cc.response.attestationObject = toBase64(newCredential.response.attestationObject);
+                cc.response.clientDataJSON = toBase64(newCredential.response.clientDataJSON);
+                cc.type = newCredential.type;
+                console.log("Sending RegisterResponse");
+                console.log(cc);
+                return fetch(REGISTER_URL + username, {
+                    method: "POST",
+                    body: JSON.stringify(cc),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+            }) // then(newC
+            // So act on the return fetch(REG_URL) now ...
+            .then(res => {
+                // Handle error?
+                if (res.status != 200) {
+                    // Update a element on page to say we broke.
+                    console.log(res);
+                    toast_o_matic("Oppsie Whoopsie");
+                    throw new Error("Opps");
+                    // How to exit properly?
+                } else {
+                    toast_o_matic("Registration Success");
+                    return res;
+                }
+            })
+            .catch(err => console.log(err, err.stack))
     })
-    .then(res => res.json())
-    .then(challenge => {
-      console.log("challenge");
-      console.log(challenge);
-      challenge.publicKey.challenge = fromBase64(challenge.publicKey.challenge);
-      challenge.publicKey.user.id = fromBase64(challenge.publicKey.user.id);
-      return navigator.credentials.create(challenge)
-        .then(newCredential => {
-          console.log("PublicKeyCredential Created");
-          console.log(newCredential);
-          console.log(typeof(newCredential));
-          const cc = {};
-          cc.id = newCredential.id;
-          cc.rawId = toBase64(newCredential.rawId);
-          cc.response = {};
-          cc.response.attestationObject = toBase64(newCredential.response.attestationObject);
-          cc.response.clientDataJSON = toBase64(newCredential.response.clientDataJSON);
-          cc.type = newCredential.type;
-          console.log("Sending RegisterResponse");
-          console.log(cc);
-          return fetch(REGISTER_URL + username, {
-            method: "POST",
-            body: JSON.stringify(cc),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-        }) // then(newC
-        // So act on the return fetch(REG_URL) now ...
-        .then(res => {
-            // Handle error?
-            if (res.status != 200) {
-                // Update a element on page to say we broke.
-                console.log(res);
-                toast_o_matic("Oppsie Whoopsie");
-                throw new Error("Opps");
-                // How to exit properly?
-            } else {
-                toast_o_matic("Registration Success");
-                return res;
-            }
-        })
-    }) // then(chal
-    .catch(err => console.log(err, err.stack))
+    .fail(function() {
+        toast_o_matic("Oppsie Whoopsie");
+    })
+
 }
 
 function login(mevent) {
   var username = document.getElementById("username").value;
-  fetch(LGN_CHALLENGE_URL + username, {method: "POST"})
-    .then(res => {
-        // Handle error?
-        if (res.status != 200) {
-            toast_o_matic("Oppsie Whoopsie Spaghettios");
-            // Update a element on page to say we broke.
-            throw new Error("Opps");
-            // How to exit properly?
-        } else {
-            return res;
-        }
-    })
-    .then(res => res.json())
-    .then(challenge => {
-      console.log("challenge");
-      console.log(challenge);
-      challenge.publicKey.challenge = fromBase64(challenge.publicKey.challenge);
-      challenge.publicKey.allowCredentials = challenge.publicKey.allowCredentials.map(c => {
-          c.id = fromBase64(c.id)
-          return c
-      });
-      console.log(challenge);
-      return navigator.credentials.get(challenge)
-        .then(credentials => {
-        // https://developer.mozilla.org/en-US/docs/Web/API/CredentialsContainer/get
-          /*
-          console.log("PublicKeyCredential Get");
-          console.log(credentials);
-          credentials.response.authenticatorData = toBase64(credentials.response.authenticatorData);
-          credentials.response.clientDataJSON = toBase64(credentials.response.clientDataJSON);
-          credentials.response.signature = toBase64(credentials.response.signature);
-          */
-          const pk = {};
-          pk.id = credentials.id;
-          pk.rawId = toBase64(credentials.rawId);
-          pk.response = {};
-          pk.response.authenticatorData = toBase64(credentials.response.authenticatorData);
-          pk.response.clientDataJSON = toBase64(credentials.response.clientDataJSON);
-          pk.response.signature = toBase64(credentials.response.signature);
-          pk.response.userHandle = toBase64(credentials.response.userHandle);
-          pk.type = credentials.type;
 
-          return fetch(LOGIN_URL + username, {
-            method: "POST",
-            body: JSON.stringify(pk),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-        }) // then(creds
-        // So act on the return fetch(LGN_URL) now ...
-        .then(res => {
-            // Handle error?
-            if (res.status != 200) {
-                // Update a element on page to say we broke.
-                console.log(res);
-                toast_o_matic("Oppsie Whoopsie Spaghettios");
-                throw new Error("Opps");
-                // How to exit properly?
-            } else {
-                toast_o_matic("Login Success");
-                console.log($.cookie("webauthnrs"));
-                return res;
-            }
-        })
-    }) // then(chal
-    .catch(err => console.log(err, err.stack))
+  jQuery.post(LGN_CHALLENGE_URL + username, null, null, 'json')
+    .done(function(challenge) {
+          console.log("challenge");
+          console.log(challenge);
+          challenge.publicKey.challenge = fromBase64(challenge.publicKey.challenge);
+          challenge.publicKey.allowCredentials = challenge.publicKey.allowCredentials.map(c => {
+              c.id = fromBase64(c.id)
+              return c
+          });
+          console.log(challenge);
+          return navigator.credentials.get(challenge)
+            .then(credentials => {
+            // https://developer.mozilla.org/en-US/docs/Web/API/CredentialsContainer/get
+              /*
+              console.log("PublicKeyCredential Get");
+              console.log(credentials);
+              credentials.response.authenticatorData = toBase64(credentials.response.authenticatorData);
+              credentials.response.clientDataJSON = toBase64(credentials.response.clientDataJSON);
+              credentials.response.signature = toBase64(credentials.response.signature);
+              */
+              const pk = {};
+              pk.id = credentials.id;
+              pk.rawId = toBase64(credentials.rawId);
+              pk.response = {};
+              pk.response.authenticatorData = toBase64(credentials.response.authenticatorData);
+              pk.response.clientDataJSON = toBase64(credentials.response.clientDataJSON);
+              pk.response.signature = toBase64(credentials.response.signature);
+              pk.response.userHandle = toBase64(credentials.response.userHandle);
+              pk.type = credentials.type;
+
+              return fetch(LOGIN_URL + username, {
+                method: "POST",
+                body: JSON.stringify(pk),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              })
+            }) // then(creds
+            // So act on the return fetch(LGN_URL) now ...
+            .then(res => {
+                // Handle error?
+                if (res.status != 200) {
+                    // Update a element on page to say we broke.
+                    console.log(res);
+                    toast_o_matic("Oppsie Whoopsie Spaghettios");
+                    throw new Error("Opps");
+                    // How to exit properly?
+                } else {
+                    toast_o_matic("Login Success");
+                    console.log($.cookie("webauthnrs"));
+                    return res;
+                }
+            }) // then(chal
+            .catch(err => console.log(err, err.stack))
+    })
+    .fail(function() {
+        toast_o_matic("Oppsie Whoopsie");
+    })
 }
 
 function toBase64(data) {
