601e7c8b59c1bc24b3503a6c2e4b56dcb01e66f6
Henry Zimmerman <hzimmerm@akamai.com>
2025-01-10 14:52:11 -0500

    add custom extensions support

--- webauthn-rs-proto/src/extensions.rs
+++ webauthn-rs-proto/src/extensions.rs
@@ -184,6 +184,10 @@ pub struct RequestAuthenticationExtensions {
     /// Hmac get secret
     #[serde(skip_serializing_if = "Option::is_none")]
     pub hmac_get_secret: Option<HmacGetSecretInput>,
+    
+    /// Custom extensions
+    #[serde(flatten)]
+    pub custom: BTreeMap<String, serde_cbor_2::Value> 
 }
 
 // Unable to create from, because it's an out of crate struct
@@ -199,6 +203,7 @@ impl Into<js_sys::Object> for &RequestAuthenticationExtensions {
             appid: _,
             uvm,
             hmac_get_secret,
+            custom: _
         } = self;
 
         let obj = Object::new();
