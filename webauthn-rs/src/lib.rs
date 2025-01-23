a60165fc6ddf824b7ff6f55c083c9d9a2bb96c72
Nikhil Tyagi <nityagi@akamai.com>
2025-01-23 12:34:47 -0500

    reg to ClientDevice

--- webauthn-rs/src/lib.rs
+++ webauthn-rs/src/lib.rs
@@ -902,7 +902,7 @@ impl Webauthn {
             .user_verification_policy(policy)
             .reject_synchronised_authenticators(false)
             .exclude_credentials(exclude_credentials)
-            .hints(Some(vec![PublicKeyCredentialHints::Hybrid]))
+            .hints(Some(vec![PublicKeyCredentialHints::ClientDevice]))
             .extensions(extensions);
 
         self.core
